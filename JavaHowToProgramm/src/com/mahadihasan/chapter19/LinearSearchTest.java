093b9253d29e043db26e64f0d91eaa4de4312604
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-12-05 19:12:17 +0600

    LinearSearchTest ---- ok

--- JavaHowToProgramm/src/com/mahadihasan/chapter19/LinearSearchTest.java
+++ JavaHowToProgramm/src/com/mahadihasan/chapter19/LinearSearchTest.java
@@ -1,15 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.mahadihasan.chapter19;
+
+import java.util.Scanner;
 
 /**
  *
  * @author MdMahadiHasan
  */
 public class LinearSearchTest {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int searchInt;
+        int position;
+
+        LinearArray searchArray = new LinearArray(10);
+
+        System.out.println("" + searchArray);
+
+        System.out.println("Please enter an integer value (-1 to quit): ");
+
+        searchInt = input.nextInt();
+
+        while (searchInt != -1) {
+
+            position = searchArray.LinearSearch(searchInt);
+
+            if (position == -1) {
+                System.out.println("The Intiger " + searchInt
+                        + " was not found in position ");
+            } else {
+                System.out.println("The integer " + searchInt
+                        + " was found in position " + position);
+            }
+
+            System.out.println("Please enter an integer value (-1 to quit): ");
+
+            searchInt = input.nextInt();
+        }
+    }
 }
