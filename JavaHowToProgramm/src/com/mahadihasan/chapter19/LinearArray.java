e5d391b7e89474641063e09968920f040405e3a5
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-12-05 19:03:56 +0600

    LinearSearch

--- JavaHowToProgramm/src/com/mahadihasan/chapter19/LinearArray.java
+++ JavaHowToProgramm/src/com/mahadihasan/chapter19/LinearArray.java
@@ -1,9 +1,36 @@
 package com.mahadihasan.chapter19;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author MdMahadiHasan
  */
 public class LinearArray {
+
+    private int[] data;
+    private static final Random generator = new Random();
+
+    public LinearArray(int size) {
+        data = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            data[ i] = 10 + generator.nextInt(90);
+        }
+    }
+
+    public int LinearSearch(int searchKey) {
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == searchKey) {
+                return index;
+            }
+        }
+        return -1;
+    }
     
+    @Override
+    public String toString() {
+        return Arrays.toString(data);
+    }
 }
